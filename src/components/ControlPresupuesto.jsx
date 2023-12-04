34839d37b120a2051b2ee5d5e7244164b0d397fa
DarkarBlays <juanpamendoza08@gmail.com>
2023-12-04 12:54:32 -0500

    first commit

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react"
+
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
+
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
+    useEffect(() => {
+        const totalGastado = gastos.reduce((total,gasto) => gasto.cantidad + total, 0);
+
+        const totalDisponible = presupuesto - totalGastado;
+
+        setDisponible(totalDisponible)
+        setGastado(totalGastado)
+
+    }, [gastos])
+
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
+    return (
+        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+
+            <div>
+                <p>Grafica aqui</p>
+            </div>
+
+            <div className="contenido-presupuesto">
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(disponible)}
+                </p>
+
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(gastado)}
+                </p>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ControlPresupuesto
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import NuevoPresupuesto from "./NuevoPresupuesto"
+import ControlPresupuesto from "./ControlPresupuesto"
+
+const Header = ({
+  presupuesto,setPresupuesto,isValidPresupuesto,setIsValidPresupuesto,gastos}) => {
+  return (
+    <header>
+
+      <h1>Planificador de Gastos</h1>
+
+      {isValidPresupuesto?(
+        <ControlPresupuesto
+          gastos={gastos}
+          presupuesto={presupuesto}
+        />
+      ):(
+
+        <NuevoPresupuesto
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}/>
+
+      )}
+      
+
+    </header>
+  )
+}
+
+export default Header
